706143c8ea236505662e134aa2633673b1d1676e
9674036-code <9674036@graniteschools.org>
2026-09-07 16:07:26 +0000

    added changing debater feature

--- src/DebateSim/DebateSim.java
+++ src/DebateSim/DebateSim.java
@@ -10,27 +10,29 @@ public class DebateSim {
     public static String[] iCalc ={"I like seeing clash and impact calc.","Clash and impact calc are very important for me.","Impact calc is how debate works and will win/lose you the round."};
     public static String[] value={"The value debate is pretty fundamental but not really extensive so I'll be a judging a bit of the round on it.","The value debate is quite fundamental so I'll weigh it considerably.","Sure impact calc is important, but the value debate is cool so I will weigh most of the round on it."};
     public static ArrayList<Debater> player = new ArrayList<>();
+    public static ArrayList<Debater> opp = new ArrayList<>();
+    public static int oppType=0;
     public static int playerType=0;
-    static{
+    public static void main(String[] args) {
         player.add(new ExtinctionDebater(0,0));
         player.add(new ProgDebater(0,0));
         player.add(new SVDebater(0,0));
-    }
-
-    public static Debater opp = new ExtinctionDebater(0,0);
-    public static void main(String[] args) {
+        opp.add(new ExtinctionDebater(0,0));
+        opp.add(new ProgDebater(0,0));
+        opp.add(new SVDebater(0,0));
+        oppType=random.nextInt(2);
         while (true){
             for (int r=0;r<5;r++){
                 judge.add(random.nextInt(60)+10);
-                opp.setMainArg(judge.get(0)+random.nextInt(18)-8);
+                opp.get(oppType).setMainArg(judge.get(0)+random.nextInt(18)-8);
                 if(judge.get(0)>31){
                     judge.add(random.nextInt(60-(judge.get(0)-30))+10);
                 }else{
                     judge.add(random.nextInt(60)+10);
                 }
-                opp.setValue(judge.get(0)+random.nextInt(18)-8);
+                opp.get(oppType).setValue(judge.get(0)+random.nextInt(18)-8);
                 judge.add(100-judge.get(0)-judge.get(1));
-                opp.setCrossX(judge.get(0)+random.nextInt(18)-8);
+                opp.get(oppType).setCrossX(judge.get(0)+random.nextInt(18)-8);
 
                 judge.add(random.nextInt(50)+10);
 
@@ -56,6 +58,11 @@ public class DebateSim {
 
                 while(true){    
                     try{
+                        System.out.println("Choose a debating style using one of the displayer integers: 1: Extinction links 2: Structural violence 3: Progressive kritiques and theory");
+                        playerType=Integer.parseInt(input.nextLine())-1;
+                        if (playerType>2 || playerType<0){
+                            throw new IndexOutOfBoundsException("Number is out of range.");
+                        }
                         System.out.println("How many points out of 100 of prepatory time do you wish to allocate to your value framework? (you will be splitting these points three ways) ");
                         player.get(playerType).setValue(Integer.parseInt(input.nextLine()));
                         System.out.println("How many points out of 100 of prepatory time do you wish to allocate towards Cross Examination? (you will be splitting these points three ways) ");
@@ -66,7 +73,7 @@ public class DebateSim {
                         break;
                         }
                         System.out.println("please use a valid integer and ensure your inputted values sum to 100");
-                    }catch(NumberFormatException e){
+                    }catch(NumberFormatException | IndexOutOfBoundsException e){
                         System.out.println("please use a valid integer and ensure your inputted values sum to 100");
                     }
                 }
@@ -77,7 +84,7 @@ public class DebateSim {
                 System.out.println("The value debate: " +judge.get(1));
                 System.out.println("Cross Examination: " +judge.get(2));
 
-                if(effectCalc(player.get(playerType).getCrossX(),judge.get(2)+effectCalc(player.get(playerType).getMainArg(),judge.get(0))+effectCalc(player.get(playerType).getValue(),judge.get(1)))<effectCalc(opp.getCrossX(),judge.get(2))+effectCalc(opp.getMainArg(),judge.get(0))+effectCalc(opp.getValue(),judge.get(1))){
+                if(effectCalc(player.get(playerType).getCrossX(),judge.get(2)+effectCalc(player.get(playerType).getMainArg(),judge.get(0))+effectCalc(player.get(playerType).getValue(),judge.get(1)))<effectCalc(opp.get(oppType).getCrossX(),judge.get(2))+effectCalc(opp.get(oppType).getMainArg(),judge.get(0))+effectCalc(opp.get(oppType).getValue(),judge.get(1))){
                     System.out.println("You have won the round.");
                     player.get(playerType).setWins(1);
                 }else{
