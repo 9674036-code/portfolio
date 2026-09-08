93d97760dc868b9ce945b94650442e4e24a0b51e
9674036-code <9674036@graniteschools.org>
2026-09-08 14:12:46 -0600

    Change mod value from 27 to 17 in SVDebater

--- src/DebateSim/SVDebater.java
+++ src/DebateSim/SVDebater.java
@@ -1,7 +1,7 @@
 public class SVDebater extends Debater{
     public static final int SPEAKER_I=80;
     public SVDebater(int w,int p) {
-        setMod(27);
+        setMod(17);
         setWins(w);
         setSpeakerP(p);
     }
